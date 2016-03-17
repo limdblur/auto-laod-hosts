af25d9b399603671428a1164c390b201f4779561
lexkde <yanjinyi@mail.sim.ac.cn>
2016-03-17 18:03:07 +0800

    给update_origin_hosts_file函数添加了返回值

--- update_origin_hosts_file.py
+++ update_origin_hosts_file.py
@@ -2,17 +2,14 @@
 #encoding:utf-8
 import subprocess
 import shlex
+import privateutil
 ORIGINHOSTS=u'/etc/hosts'
 
-def get_info_from_config_file():
-    configfp=open('hosts_urls_date.txt')
-    lines=configfp.readlines()
-    configfp.close()
-    return (lines[1].split(':')[1][0:-1],lines[2].split(':')[1][0:-1])
+
 
 def update_origin_hosts_file():
     #backup
-    (hostsfile_update_date,hostsfile_update_version)=get_info_from_config_file()
+    (hostsfile_update_date,hostsfile_update_version)=privateutil.get_hosts_info_from_config_file()
     commandline='cp /etc/hosts /etc/hosts-'+hostsfile_update_date+'-v'+hostsfile_update_version
     args=shlex.split(commandline)
     try:
@@ -29,6 +26,7 @@ def update_origin_hosts_file():
     except Exception,e:
         print '执行覆盖失败',e
         return False
+    return True
         
 if __name__=='__main__':
     update_origin_hosts_file()
